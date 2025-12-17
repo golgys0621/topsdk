cd3bc9e3c537b318b7a8d7112fbe5766a5a8be37
LGYS <18207468101@qq.com>
2025-12-17 15:55:36 +0800

    整体更新

---
+++ defaultability/response/AlibabaAlihealthDrugCodeKytWesCheckcoderelationallResponse.go
@@ -0,0 +1,35 @@
+package response
+
+import (
+	"github.com/golgys0621/topsdk/defaultability/domain"
+)
+
+type AlibabaAlihealthDrugCodeKytWesCheckcoderelationallResponse struct {
+
+	/*
+	   System request id
+	*/
+	RequestId string `json:"request_id,omitempty" `
+
+	/*
+	   System body
+	*/
+	Body string
+
+	/*
+	   返回的码的结果
+	*/
+	Model domain.AlibabaAlihealthDrugCodeKytWesCheckcoderelationallWesCodeRelationDTOV2 `json:"model,omitempty" `
+	/*
+	   返回信息
+	*/
+	MsgInfo string `json:"msg_info,omitempty" `
+	/*
+	   返回码
+	*/
+	MsgCode string `json:"msg_code,omitempty" `
+	/*
+	   调用是否成功
+	*/
+	ResponseSuccess bool `json:"response_success,omitempty" `
+}
