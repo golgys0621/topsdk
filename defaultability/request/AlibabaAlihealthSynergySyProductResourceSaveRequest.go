1943ba52ac50162a6a2157e8e95556700fa68512
LGYS <18207468101@qq.com>
2026-09-24 18:15:25 +0800

    defaultability

--- defaultability/request/AlibabaAlihealthSynergySyProductResourceSaveRequest.go
+++ defaultability/request/AlibabaAlihealthSynergySyProductResourceSaveRequest.go
@@ -1,95 +1,119 @@
 package request
 
+import (
+        "github.com/golgys0621/topsdk/util"
+    )
+
 type AlibabaAlihealthSynergySyProductResourceSaveRequest struct {
-	/*
-	   备注     */
-	Note *string `json:"note,omitempty" required:"false" `
-	/*
-	   首营产品id     */
-	SyProductId *int64 `json:"sy_product_id" required:"true" `
-	/*
-	   资料名称     */
-	ResourceName *string `json:"resource_name" required:"true" `
-	/*
-	   有效期，长期有效传“长期”     */
-	ExpireDate *string `json:"expire_date" required:"true" `
-	/*
-	   refEntId     */
-	RefEntId *string `json:"ref_ent_id" required:"true" `
-	/*
-	   资料类别     */
-	ResourceTypeCode *string `json:"resource_type_code" required:"true" `
-	/*
-	   文件类型，只支持"pdf", "jpg", "jpeg", "png","tif"     */
-	FileType *string `json:"file_type" required:"true" `
-	/*
-	   文件内容     */
-	FileContent *[]byte `json:"file_content" required:"true" `
+    /*
+        备注     */
+    Note  *string `json:"note,omitempty" required:"false" `
+    /*
+        首营产品id     */
+    SyProductId  *int64 `json:"sy_product_id" required:"true" `
+    /*
+        资料名称     */
+    ResourceName  *string `json:"resource_name" required:"true" `
+    /*
+        有效期，长期有效传“长期”     */
+    ExpireDate  *string `json:"expire_date" required:"true" `
+    /*
+        refEntId     */
+    RefEntId  *string `json:"ref_ent_id" required:"true" `
+    /*
+        资料类别，选择AI识别时不要传递     */
+    ResourceTypeCode  *string `json:"resource_type_code,omitempty" required:"false" `
+    /*
+        文件类型，只支持"pdf", "jpg", "jpeg", "png","tif"     */
+    FileType  *string `json:"file_type" required:"true" `
+    /*
+        资料文件与fileUrl字段填写一个即可     */
+    FileContent  *[]byte `json:"file_content,omitempty" required:"false" `
+    /*
+        上传文件的httpurl地址     */
+    FileUrl  *string `json:"file_url,omitempty" required:"false" `
+    /*
+        1:资料类型由ai识别获取     */
+    NeedAi  *string `json:"need_ai,omitempty" required:"false" `
 }
 
 func (s *AlibabaAlihealthSynergySyProductResourceSaveRequest) SetNote(v string) *AlibabaAlihealthSynergySyProductResourceSaveRequest {
-	s.Note = &v
-	return s
+    s.Note = &v
+    return s
 }
 func (s *AlibabaAlihealthSynergySyProductResourceSaveRequest) SetSyProductId(v int64) *AlibabaAlihealthSynergySyProductResourceSaveRequest {
-	s.SyProductId = &v
-	return s
+    s.SyProductId = &v
+    return s
 }
 func (s *AlibabaAlihealthSynergySyProductResourceSaveRequest) SetResourceName(v string) *AlibabaAlihealthSynergySyProductResourceSaveRequest {
-	s.ResourceName = &v
-	return s
+    s.ResourceName = &v
+    return s
 }
 func (s *AlibabaAlihealthSynergySyProductResourceSaveRequest) SetExpireDate(v string) *AlibabaAlihealthSynergySyProductResourceSaveRequest {
-	s.ExpireDate = &v
-	return s
+    s.ExpireDate = &v
+    return s
 }
 func (s *AlibabaAlihealthSynergySyProductResourceSaveRequest) SetRefEntId(v string) *AlibabaAlihealthSynergySyProductResourceSaveRequest {
-	s.RefEntId = &v
-	return s
+    s.RefEntId = &v
+    return s
 }
 func (s *AlibabaAlihealthSynergySyProductResourceSaveRequest) SetResourceTypeCode(v string) *AlibabaAlihealthSynergySyProductResourceSaveRequest {
-	s.ResourceTypeCode = &v
-	return s
+    s.ResourceTypeCode = &v
+    return s
 }
 func (s *AlibabaAlihealthSynergySyProductResourceSaveRequest) SetFileType(v string) *AlibabaAlihealthSynergySyProductResourceSaveRequest {
-	s.FileType = &v
-	return s
+    s.FileType = &v
+    return s
 }
 func (s *AlibabaAlihealthSynergySyProductResourceSaveRequest) SetFileContent(v []byte) *AlibabaAlihealthSynergySyProductResourceSaveRequest {
-	s.FileContent = &v
-	return s
+    s.FileContent = &v
+    return s
+}
+func (s *AlibabaAlihealthSynergySyProductResourceSaveRequest) SetFileUrl(v string) *AlibabaAlihealthSynergySyProductResourceSaveRequest {
+    s.FileUrl = &v
+    return s
+}
+func (s *AlibabaAlihealthSynergySyProductResourceSaveRequest) SetNeedAi(v string) *AlibabaAlihealthSynergySyProductResourceSaveRequest {
+    s.NeedAi = &v
+    return s
 }
 
 func (req *AlibabaAlihealthSynergySyProductResourceSaveRequest) ToMap() map[string]interface{} {
-	paramMap := make(map[string]interface{})
-	if req.Note != nil {
-		paramMap["note"] = *req.Note
-	}
-	if req.SyProductId != nil {
-		paramMap["sy_product_id"] = *req.SyProductId
-	}
-	if req.ResourceName != nil {
-		paramMap["resource_name"] = *req.ResourceName
-	}
-	if req.ExpireDate != nil {
-		paramMap["expire_date"] = *req.ExpireDate
-	}
-	if req.RefEntId != nil {
-		paramMap["ref_ent_id"] = *req.RefEntId
-	}
-	if req.ResourceTypeCode != nil {
-		paramMap["resource_type_code"] = *req.ResourceTypeCode
-	}
-	if req.FileType != nil {
-		paramMap["file_type"] = *req.FileType
-	}
-	return paramMap
+    paramMap := make(map[string]interface{})
+    if(req.Note != nil) {
+        paramMap["note"] = *req.Note
+    }
+    if(req.SyProductId != nil) {
+        paramMap["sy_product_id"] = *req.SyProductId
+    }
+    if(req.ResourceName != nil) {
+        paramMap["resource_name"] = *req.ResourceName
+    }
+    if(req.ExpireDate != nil) {
+        paramMap["expire_date"] = *req.ExpireDate
+    }
+    if(req.RefEntId != nil) {
+        paramMap["ref_ent_id"] = *req.RefEntId
+    }
+    if(req.ResourceTypeCode != nil) {
+        paramMap["resource_type_code"] = *req.ResourceTypeCode
+    }
+    if(req.FileType != nil) {
+        paramMap["file_type"] = *req.FileType
+    }
+    if(req.FileUrl != nil) {
+        paramMap["file_url"] = *req.FileUrl
+    }
+    if(req.NeedAi != nil) {
+        paramMap["need_ai"] = *req.NeedAi
+    }
+    return paramMap
 }
 
 func (req *AlibabaAlihealthSynergySyProductResourceSaveRequest) ToFileMap() map[string]interface{} {
-	fileMap := make(map[string]interface{})
-	if req.FileContent != nil {
-		fileMap["file_content"] = *req.FileContent
-	}
-	return fileMap
+    fileMap := make(map[string]interface{})
+    if req.FileContent != nil {
+        fileMap["file_content"] = *req.FileContent
+    }
+    return fileMap
 }
--- defaultability/request/AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest.go
+++ defaultability/request/AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest.go
@@ -1,60 +1,80 @@
 package request
 
 import (
-	"github.com/golgys0621/topsdk/defaultability/domain"
-	"github.com/golgys0621/topsdk/util"
-)
+        "github.com/golgys0621/topsdk/defaultability/domain"
+        "github.com/golgys0621/topsdk/util"
+    )
 
 type AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest struct {
-	/*
-	   企业（本企业id）     */
-	RefEntId *string `json:"ref_ent_id" required:"true" `
-	/*
-	   上游出库单开始日期（最大查询区间一月）     */
-	BeginTime *string `json:"begin_time" required:"true" `
-	/*
-	   上游出库单结束日期（最大查询区间一月）     */
-	EndTime *string `json:"end_time" required:"true" `
-	/*
-	   分页对象     */
-	Page *domain.AlibabaAlihealthSynergyYzwSignednotstampedbillQueryPage `json:"page" required:"true" `
+    /*
+        企业（本企业id）     */
+    RefEntId  *string `json:"ref_ent_id" required:"true" `
+    /*
+        上游出库单开始日期（最大查询区间一月）     */
+    BeginTime  *string `json:"begin_time" required:"true" `
+    /*
+        上游出库单结束日期（最大查询区间一月）     */
+    EndTime  *string `json:"end_time" required:"true" `
+    /*
+        批次号     */
+    BatchNo  *string `json:"batch_no,omitempty" required:"false" `
+    /*
+        药品id     */
+    DrugId  *string `json:"drug_id,omitempty" required:"false" `
+    /*
+        分页对象     */
+    Page  *domain.AlibabaAlihealthSynergyYzwSignednotstampedbillQueryPage `json:"page" required:"true" `
 }
 
 func (s *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest) SetRefEntId(v string) *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest {
-	s.RefEntId = &v
-	return s
+    s.RefEntId = &v
+    return s
 }
 func (s *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest) SetBeginTime(v string) *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest {
-	s.BeginTime = &v
-	return s
+    s.BeginTime = &v
+    return s
 }
 func (s *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest) SetEndTime(v string) *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest {
-	s.EndTime = &v
-	return s
+    s.EndTime = &v
+    return s
+}
+func (s *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest) SetBatchNo(v string) *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest {
+    s.BatchNo = &v
+    return s
+}
+func (s *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest) SetDrugId(v string) *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest {
+    s.DrugId = &v
+    return s
 }
 func (s *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest) SetPage(v domain.AlibabaAlihealthSynergyYzwSignednotstampedbillQueryPage) *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest {
-	s.Page = &v
-	return s
+    s.Page = &v
+    return s
 }
 
 func (req *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest) ToMap() map[string]interface{} {
-	paramMap := make(map[string]interface{})
-	if req.RefEntId != nil {
-		paramMap["ref_ent_id"] = *req.RefEntId
-	}
-	if req.BeginTime != nil {
-		paramMap["begin_time"] = *req.BeginTime
-	}
-	if req.EndTime != nil {
-		paramMap["end_time"] = *req.EndTime
-	}
-	if req.Page != nil {
-		paramMap["page"] = util.ConvertStruct(*req.Page)
-	}
-	return paramMap
+    paramMap := make(map[string]interface{})
+    if(req.RefEntId != nil) {
+        paramMap["ref_ent_id"] = *req.RefEntId
+    }
+    if(req.BeginTime != nil) {
+        paramMap["begin_time"] = *req.BeginTime
+    }
+    if(req.EndTime != nil) {
+        paramMap["end_time"] = *req.EndTime
+    }
+    if(req.BatchNo != nil) {
+        paramMap["batch_no"] = *req.BatchNo
+    }
+    if(req.DrugId != nil) {
+        paramMap["drug_id"] = *req.DrugId
+    }
+    if(req.Page != nil) {
+        paramMap["page"] = util.ConvertStruct(*req.Page)
+    }
+    return paramMap
 }
 
 func (req *AlibabaAlihealthSynergyYzwSignednotstampedbillQueryRequest) ToFileMap() map[string]interface{} {
-	fileMap := make(map[string]interface{})
-	return fileMap
+    fileMap := make(map[string]interface{})
+    return fileMap
 }
