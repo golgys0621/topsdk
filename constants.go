2ef0058d52ebb15c7fc4a2f712c3a74dcb6bf1ab
LGYS <18207468101@qq.com>
2026-09-24 17:36:19 +0800

    码上放心

--- constants.go
+++ constants.go
@@ -5,7 +5,7 @@ package topsdk
  */
 const (
 	// SdkVersion 版本号
-	SdkVersion = "new_go_sdk_20251217"
+	SdkVersion = "new_go_sdk_20260924"
 
 	// ApiFormat api格式
 	ApiFormat = "json"
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/golgys0621/topsdk
 
-go 1.25
+go 1.27
